2ae87948cb5127eb7ae6284e938a0e8ec698ff04
Duc Tran <901stt@gmail.com>
2016-11-24 22:34:11 +0700

    Combine all install packages to one line Combine all install packages to one line

--- install_linux_pakages.bash
+++ install_linux_pakages.bash
@@ -2,7 +2,5 @@
 
 # ArchLinux
 # idutils in AUR
-pacman -Syu --needed ctags python-pygments cabal-install stack ghc emacs auctex emacs-haskell-mode emacs-lua-mode emacs-python-mode
-
-# Haskell
-pacman -Su --needed happy alex ghc-mod haskell-ghc-mod hasktags stylish-haskell hlint hoogle hindent
+pacman -Syu --needed ctags python-pygments cabal-install stack ghc emacs auctex emacs-haskell-mode emacs-lua-mode emacs-python-mode \
+  happy alex ghc-mod haskell-ghc-mod hasktags stylish-haskell hlint hoogle hindent   # Haskell
